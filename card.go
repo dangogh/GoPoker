c576eeace766746f042a0dc3ef69bcfc6e106081
Dan Kirkwood <dangogh@gmail.com>
2012-08-15 15:38:00 -0600

    straight detection

--- card.go
+++ card.go
@@ -1,77 +1,79 @@
 package poker
 
-import (
-	"strings"
-	"errors"
-	"fmt"
-	)
-
 type Card struct {
-	name       string
 	rank, suit rune
 }
 
 var (
-	ranks = "23456789TJQKA"
-	suits = "HDSC"
-)
+	// string of rank/suit values in increasing value order
+	rankValues, suitValues string
+	// map of rank/suit to index
+	ranks, suits map[rune]int
+    )
 
+func init() {
+	rankValues = "23456789TJQKA"
+	ranks = make(map[rune]int, len(rankValues))
+	for ii, r := range rankValues {
+		ranks[r] = ii
+	}
+	suitValues = "CSDH"
+	suits = make(map[rune]int, len(suitValues))
+	for ii, s := range suitValues {
+		suits[s] = ii
+	}
+}
+
+func NewCard(name string) (c *Card, err bool) {
+	// search for suit and rank in letters of name.
+	// Order is not important
+	var rank, suit rune
+	for _, let := range name {
+		if _, found := ranks[let]; found {
+			rank = let
+		} else if _, found := suits[let]; found {
+			suit = let
+		}
+	}
+	c = new(Card)
+	c.rank, c.suit = rank, suit
+	return c, false
+}
+
+func (c Card) RankIndex() int {
+	return ranks[c.rank]
+}
+
+func (c Card) SuitIndex() int {
+	return suits[c.suit]
+}
+
+
+// Class methods
 func Suits() []rune {
-  var runes []rune
-  for _, r := range strings.Split(suits, "") {
-  	runes = append(runes, rune(r[0]))
-  }
-  return runes
+	var suitAry []rune
+	for _, suit := range suitValues {
+		suitAry = append(suitAry, suit)
+	}
+	return suitAry
 }
 
 func Ranks() []rune {
-  var runes []rune
-  for _, r := range strings.Split(ranks, "") {
-  	runes = append(runes, rune(r[0]))
-  }
-  return runes
-}
-
-func (c Card) extrude() (err error) {
-	isuit, irank := -1, -1
-	if len(c.name) == 2 {
-		for _, ch := range c.name {
-			if isuit != -1 && irank != -1 {
-				break
-			}
-			if isuit == -1 {
-				isuit = strings.Index(suits, string(ch))
-				fmt.Printf("isuit is %v\n", isuit)
-			}
-			if irank == -1 {
-				irank = strings.Index(ranks, string(ch))
-				fmt.Printf("irank is %v\n", irank)
-			}
-		}
+	var rankAry []rune
+	for _, rank := range rankValues {
+		rankAry = append(rankAry, rank)
 	}
-	if irank == -1 || isuit == -1 {
-		return errors.New("Error creating card from " + c.name)
-	}
-
-	c.rank, c.suit = rune(ranks[irank]), rune(suits[isuit])
-	return nil
+	return rankAry
 }
 
 func (c Card) Rank() rune {
-	if c.rank == 0 {
-		c.extrude()
-	}
 	return c.rank
 }
 
 func (c Card) Suit() rune {
-	if c.suit == 0 {
-		c.extrude()
-	}
 	return c.suit
 }
 
 func (c Card) String() string {
-	ret := string(c.Rank()) + string(c.Suit())
-	return ret
+	return string(c.Rank()) + string(c.Suit())
 }
--- hand.go
+++ hand.go
@@ -1,3 +1,31 @@
 package poker
-
 type Hand []Card
+
+func (h Hand) IsStraight() bool {
+	low, high := 999, -1
+	for _, c := range h {
+		val := c.RankIndex()
+		if val < low {
+			low = val
+		}
+		if val > high {
+			high = val
+		}
+	}
+	return high - low + 1 == len(h)
+}
+
+func (h Hand) IsFlush() bool {
+	suit := h[0].Suit()
+	for _, c := range h {
+		val := c.Suit()
+		if val != suit {
+			return false
+		}
+	}
+	return true
+}
+
+func (h Hand) HighCard() Card {
+	return h[0]
+}
